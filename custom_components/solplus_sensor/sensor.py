802f76e767112fe8034973b95d00210bca2bd316
Jonas Kell <jonas-kell@web.de>
2023-08-09 16:25:47 +0200

    fix: init variable

--- custom_components/solplus_sensor/sensor.py
+++ custom_components/solplus_sensor/sensor.py
@@ -215,7 +215,7 @@ class InverterSensor(RestoreSensor):
         self._sensor_type = sensor_type
 
         self._native_value = 0
-        self._value_loaded_from = None
+        self._store_last_reset = datetime.min
 
         self._has_loaded_once = False  # for RestoreSensor features
 
